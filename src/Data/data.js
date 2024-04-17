0eb700dfb0d1c29c7e04e3f4fb4b5070ee631761
infolanze807 <techteam.infolanze@gmail.com>
2024-04-17 11:30:52 +0530

    latest first

--- src/Data/data.js
+++ src/Data/data.js
@@ -17,7 +17,7 @@ const ProjectData =
         detail:"Inferenz Tech Private limited, Ahmedabad, India ",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, culpa reprehenderit odio consequatur quibusdam, facilis aliquam libero animi at aperiam voluptatibus sit, dignissimos itaque provident. Ex, dolore quibusdam? Deserunt,",
         img:nft,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
 
     },
     {
@@ -26,7 +26,7 @@ const ProjectData =
         detail:"Inferenz Tech Private Limited, Ahmedabad, India",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, culpa reprehenderit odio consequatur quibusdam, facilis aliquam libero animi at aperiam voluptatibus sit, dignissimos itaque provident. Ex, dolore quibusdam? Deserunt,",
         img:meditracker,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"3",
@@ -34,7 +34,7 @@ const ProjectData =
         detail:"",
         description:"Vera Rental is an innovative project that allows users to rent out NFTs (non-fungible tokens) for a specific period of time. This new concept is perfect for collectors who want to share their valuable digital assets with others, or for those who want to experience owning an NFT without committing to a long-term purchase. With Vera Rental, you can explore the world of NFTs in a whole new way.",
         img:vera,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"4",
@@ -42,7 +42,7 @@ const ProjectData =
         detail:"",
         description:"Ethermax is an exciting new project that showcases the power of blockchain technology. With its blockchain-based reward claim feature, Ethermax allows users to easily claim rewards via their digital wallet. This innovative project is a perfect example of how blockchain technology can be used to create new and exciting opportunities for businesses and individuals alike.",
         img:ether,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"5",
@@ -50,7 +50,7 @@ const ProjectData =
         detail:"",
         description:"SkillUp is a cutting-edge e-learning platform that has been recently launched to provide a seamless learning experience for both educators and learners. With features such as course designing, material uploading, and student progress tracking, SkillUp ensures that instructors can easily manage their courses while students can have access to a diverse range of courses based on their membership category.",
         img:skill,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"6",
@@ -58,7 +58,7 @@ const ProjectData =
         detail:"",
         description:"DigiStream is an innovative online video streaming app that revolutionizes the way we consume digital content.With seamless integration of React.js and IPFS, DigiStream offers a decentralized storage solution, ensuring privacy, reliability, and scalability",
         img:digistram,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"7",
@@ -66,7 +66,7 @@ const ProjectData =
         detail:"",
         description:"The Opinion Poll System on the blockchain serves as a sophisticated and decentralized mechanism for the evaluation of professors within institutions. This innovative solution leverages blockchain technology to enhance transparency, security, and reliability in the assessment process The system encompasses three pivotal user roles: Admin, Professors, and Students, ensuring a seamless and accountable",
         img:opinion,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"8",
@@ -74,7 +74,7 @@ const ProjectData =
         detail:"",
         description:"",
         img:ecom1,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"9",
@@ -82,7 +82,7 @@ const ProjectData =
         detail:"",
         description:"",
         img:ecom2,
-        website:"www.infolanze.com",
+        website:'https://www.infolanze.tech',
     },
     {
         id:"10",
@@ -90,7 +90,7 @@ const ProjectData =
         detail:"",
         description:"",
         img:lead,
-        website:"www.leadhunter.co.in",
+        website:'https://www.leadhunter.co.in',
     },
 ];
 export default ProjectData
